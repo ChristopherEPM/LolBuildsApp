a952673d3cb5a61888d365691e8d71fba22248f0
ChristopherEPM <bloodaxe00@hotmail.com>
2016-03-13 21:17:39 +0100

    add the site.js and site controller. with the show method. Site.js show all the champs

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,153 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+//var dev = ENV["DEV_API_KEY"];
+//var dev2 = Figaro.env.dev_api_key;
+//console.log(dev);
+//console.log(dev2);
+var ajaxRequest = function (){
+  console.log("Starting ajax Request ");
+  $.ajax({
+    type: 'GET',
+    url:'https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?api_key=754c3074-aa73-40e7-9404-68f77b1f79d2',
+    success: renderResults,
+    error: ajaxRequestError
+  });
+  //$.get('https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?api_key=754c3074-aa73-40e7-9404-68f77b1f79d2').done(renderResults);
+};
+
+var renderResults = function(requestResultsAsJson){
+  console.log(requestResultsAsJson.data);
+  renderChampionName(requestResultsAsJson.data);
+}
+
+
+var ajaxRequestError = function(requestResultsAsJson){
+  console.log(requestResultsAsJson);
+}
+
+var renderChampionName = function(championsData){ 
+  // championsData.forEach( function appendName(element){
+  //   $(".champions-grid").append('<p>'+element.name+'</p>');
+  // });
+  for(var index in championsData){
+    $(".champions-grid").append('<p>'+championsData[index].name+'</p>');
+  }
+  // $.each(championsData, function appendName(element){
+  //    //console.log(, i);
+  //   $(".champions-grid").append('<p>'+element.name+'</p>');
+  // });
+}
+$(document).ready(function(){ //handlers
+  ajaxRequest();
+});
+
+
+
+
+// var ajaxRequest = function(termSearch){
+//   console.log("rquest intro")
+//   //$.get('https://api.spotify.com/v1/search?q='+termSearch+'&type=track').done(renderResults);
+//   $.ajax({
+//     url: 'https://api.spotify.com/v1/search?q='+termSearch+'&type=track',
+//     success: renderResults,
+//     error: ajaxRequestError
+//   });
+// }
+
+// var renderResults = function(searchResultsAsJson){
+//   console.log(searchResultsAsJson);
+//   //debugger;
+//   console.log("hi");
+//   if(searchResultsAsJson.tracks.items.length > 0){
+//     cleanDOMTags();
+//     var arrArtists =[]; 
+//     searchResultsAsJson.tracks.items[0].artists.forEach( function(artist) {
+//       arrArtists.push(artist.name);
+//     });
+//     var albumImageUrl = searchResultsAsJson.tracks.items[0].album.images[1].url;
+//     var trackPreviewUrl = searchResultsAsJson.tracks.items[0].preview_url;
+//     var trackName = searchResultsAsJson.tracks.items[0].name;
+
+//     renderTrackName(trackName);
+//     renderArtistsName(arrArtists);
+//     renderAlbumImage(albumImageUrl);
+//     renderTrackPreview(trackPreviewUrl);
+//   }
+//   else{
+//     alert("No search results");
+//   }
+// }
+
+// var renderTrackName = function(trackName){
+//   $('.title').append(trackName);
+// } 
+// var renderArtistsName = function(arrArtists){
+//   arrArtists.forEach( function(artistName) {
+//     $('.author').append(artistName+" . ")
+//   });
+// }
+// var renderAlbumImage = function(albumImageUrl){
+//   $('.js-almbum-img-source').attr('src',albumImageUrl);
+// }
+// var renderTrackPreview =  function(trackPreviewUrl){
+//   $('.js-player').attr('src',trackPreviewUrl);
+//   $('.btn-play').removeClass('disabled');
+// }
+// var trackClassToggler = function(){
+//   $('.btn-play').toggleClass('playing');
+// }
+
+// var playerTrigger = function(){
+//   if($('.btn-play').hasClass('playing')){
+//      $('.js-player').trigger('play');
+//   }
+//   else{
+//     $('.js-player').trigger('pause');
+//   }
+// }
+
+// var printTime = function () {
+//   var current = $('.js-player').prop('currentTime');
+//   console.debug('Current time: ' + current);
+//   $('.js-progress-bar').val(current);
+// }
+
+// var cleanDOMTags =  function(){
+
+//   $('.js-almbum-img-source').attr('src',"");
+//   $('.js-track-audio-source').attr('src',"");
+//   $('.author').empty();
+//   $('.title').empty();
+//   $('.js-progress-bar').val(0);
+// }
+
+// var ajaxRequestError = function(){
+
+// }
+
+
+// $(document).ready(function(){
+//   //events handlers
+
+//   var doSearch = function(){
+
+//     $(".btn-submit").on("click", function(event){
+//       event.preventDefault();// evitamos que haga el submit y recargue la pagina
+//       var termSearch = $('.js-sinput').val();
+//       ajaxRequest(termSearch);
+//     });
+
+//   }();
+
+//   var playerStateHandler = function(){
+//     $('.btn-play').on('click', function(){
+//       trackClassToggler();
+//       playerTrigger();
+//     });
+//   }();
+
+//   var playerTimeUpdateHandler = function(){
+//     $('.js-player').on('timeupdate', printTime);
+//   }();
+
+// });
---
+++ app/models/riot_api.rb
@@ -0,0 +1,25 @@
+class RiotApi < ActiveRecord::Base
+
+  RIOT_API_URL = "https://euw.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Biman?api_key=#{ENV['dev_api_key']}"
+  REQUEST_DEFAULTS = { :parse? => true }
+
+  # def self.uri(path)
+  #   url_string = "#{POKEMON_API_URL}/#{path}/"
+  #   return URI.parse(url_string)
+  # end
+
+  # def self.request(path, given_options = {})
+  #   options = REQUEST_DEFAULTS.merge(given_options)
+
+  #   uri = self.uri(path)
+  #   response = Net::HTTP.get_response(uri)
+  #   body = response.body
+
+  #   if options[:parse?]
+  #     body = JSON.parse(body)
+  #   end
+
+  #   return body
+  # end
+
+end
