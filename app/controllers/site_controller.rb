c5abbef75f1bea255f1c6ae17bc626ae687153da
ChristopherEPM <bloodaxe00@hotmail.com>
2016-03-16 09:18:09 +0100

    swapped the rio_api_dev_key value for a enviroment

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,16 @@
+require 'lol'
 class SiteController < ApplicationController
-
+  
+  
+  
   def home
     
   end
+
+  def show_champions_greed 
+    client = Lol::Client.new ENV["RIOT_DEV_API_KEY"], {region: "euw"}
+    @champions =  client.static.champion.get(champData: 'image')
+    render 'champions_grid'
+  end
+
 end
