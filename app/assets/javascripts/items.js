5bbb6daa6df32383867db78907b4c23ce32d2aaa
ChristopherEPM <bloodaxe00@hotmail.com>
2016-03-23 20:07:05 +0100

    add js, scss, controller,helper,spec, and a view/partial for item, the js file make calcs to give us all the items stats on click

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,206 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+Number.prototype.toFixedDown = function(digits) {
+    var re = new RegExp("(\\d+\\.\\d{" + digits + "})(\\d)"),
+        m = this.toString().match(re);
+    return m ? parseFloat(m[1]) : this.valueOf();
+};
+
+var extractItemStats = function(itemIndex){
+
+  var actualBackPack = JSON.parse(window.localStorage.getItem("actualBackPackItemsStats"));
+  actualBackPack[itemIndex] = null;
+  //actualBackPack.splice(itemIndex, 1);
+  window.localStorage.setItem("actualBackPackItemsStats", JSON.stringify(actualBackPack));
+  var actualBackPack = JSON.parse(window.localStorage.getItem("actualBackPackItemsStats"));
+  setTotalStats();
+}
+var getOutBackPackitem = function(item){
+  $(item).remove();
+}
+
+var putBackPackItem = function(image,itemStats,itemDescription,itemId){ 
+  $('[class*=js-backpack-item]').each(function(index){
+    if($(this).attr('src')==""){
+      $(this).attr('src', image);
+      $(this).attr('data-stats', itemStats);
+      $(this).attr('data-description', itemDescription);
+      $(this).attr('data-itemId', itemId);
+        return false;
+    }
+  });
+}
+var renderItemBackPackImage = function(image, itemIndexPosition){
+  var itemImage = '<img src="http://ddragon.leagueoflegends.com/cdn/6.5.1/img/item/'+image+'" class="js-backpack-item" data-indexPosition="'+itemIndexPosition+'">';
+  $('.js-backpack').append(itemImage);
+
+   $('.js-backpack-item').on('click', function(){
+        var itemIndex = $(this).attr('data-indexPosition');
+        $(this).remove();
+        extractItemStats(itemIndex);
+    });
+
+}
+var itemIsUnique = function(itemId){
+  $('[class*=js-backpack-item]').each(function(index){
+    if($(this).attr('data-itemId')==itemId){
+        return false;
+    }
+  });
+}
+
+var getItemStats =  function(itemStats){
+  var totalStats = { PercentAttackSpeed: 0, FlatMagicDamage:0, FlatPhysicalDamage: 0, 
+    FlatMovementSpeed: 0, PercentMovementSpeed: 0, FlatCritChance: 0, FlatArmor: 0, FlatSpellBlock: 0, 
+    FlatHPPool: 0, FlatHPRegen: 0, FlatMPPool: 0, PercentLifeSteal: 0 };
+  if("PercentAttackSpeedMod" in itemStats ){
+    totalStats["PercentAttackSpeed"] += itemStats["PercentAttackSpeedMod"];
+  }
+  if("FlatMagicDamageMod" in itemStats ){
+    totalStats["FlatMagicDamage"] += itemStats["FlatMagicDamageMod"];
+  }
+  if("FlatPhysicalDamageMod" in itemStats ){
+    totalStats["FlatPhysicalDamage"] += itemStats["FlatPhysicalDamageMod"];
+  }
+  if("FlatMovementSpeedMod" in itemStats ){
+    totalStats["FlatMovementSpeed"] += itemStats["FlatMovementSpeedMod"];
+    if("PercentMovementSpeed" in itemStats){
+      totalStats["FlatMovementSpeed"] = (totalStats["FlatMovementSpeed"] * totalStats["PercentMovementSpeed"]) + totalStats["FlatMovementSpeed"];
+    }
+  }
+  if("PercentMovementSpeedMod" in itemStats ){
+    totalStats["PercentMovementSpeed"] += itemStats["PercentMovementSpeedMod"];
+    if("FlatMovementSpeed" in itemStats && totalStats["FlatMovementSpeed"] > 0){
+      totalStats["FlatMovementSpeed"] = (totalStats["FlatMovementSpeed"] * totalStats["PercentMovementSpeed"]) + totalStats["FlatMovementSpeed"];
+    }
+  }
+  if("FlatCritChanceMod" in itemStats ){
+    totalStats["FlatCritChance"] += itemStats["FlatCritChanceMod"];
+  }
+  if("FlatArmorMod" in itemStats ){
+    totalStats["FlatArmor"] += itemStats["FlatArmorMod"];
+  }
+  if("FlatSpellBlockMod" in itemStats ){
+    totalStats["FlatSpellBlock"] += itemStats["FlatSpellBlockMod"];
+  }
+  if("FlatHPPoolMod" in itemStats ){
+    totalStats["FlatHPPool"] += itemStats["FlatHPPoolMod"];
+  }
+  if("FlatHPRegenMod" in itemStats ){
+    totalStats["FlatHPRegen"] += itemStats["FlatHPRegenMod"];
+  }
+  if("FlatMPPoolMod" in itemStats ){
+    totalStats["FlatMPPool"] += itemStats["FlatMPPoolMod"];
+  }
+  if("PercentLifeStealMod" in itemStats ){
+    totalStats["PercentLifeSteal"] += itemStats["PercentLifeStealMod"];
+  }
+  return totalStats;
+}
+var getTotalStats = function(arrayStats){
+  var itemsTotalStats = { PercentAttackSpeed: 0, FlatMagicDamage:0, FlatPhysicalDamage: 0, 
+    FlatMovementSpeed: 0, PercentMovementSpeed: 0, FlatCritChance: 0, FlatArmor: 0, FlatSpellBlock: 0, 
+    FlatHPPool: 0, FlatHPRegen: 0, FlatMPPool: 0, PercentLifeSteal: 0 };
+  arrayStats.forEach( function(element) {
+    itemsTotalStats["PercentAttackSpeed"] += element["PercentAttackSpeed"];
+    itemsTotalStats["FlatMagicDamage"] += element["FlatMagicDamage"];
+    itemsTotalStats["FlatPhysicalDamage"] += element["FlatPhysicalDamage"];
+    itemsTotalStats["FlatMovementSpeed"] += element["FlatMovementSpeed"];
+    itemsTotalStats["PercentMovementSpeed"] += element["PercentMovementSpeed"];
+    itemsTotalStats["FlatCritChance"] += element["FlatCritChance"];
+    itemsTotalStats["FlatArmor"] += element["FlatArmor"];
+    itemsTotalStats["FlatSpellBlock"] += element["FlatSpellBlock"];
+    itemsTotalStats["FlatHPPool"] += element["FlatHPPool"];
+    itemsTotalStats["FlatHPRegen"] += element["FlatHPRegen"];
+    itemsTotalStats["FlatMPPool"] += element["FlatMPPool"];
+    itemsTotalStats["PercentLifeSteal"] += element["PercentLifeSteal"];
+
+  });
+  return itemsTotalStats;
+}
+var renderAllStats = function(arrayTotalStats){
+   $('.js-percentAttackSpeed').text(arrayTotalStats["PercentAttackSpeed"].toFixedDown(3));
+   $('.js-percentAttackSpeed').attr('data-statsAmount', arrayTotalStats["PercentAttackSpeed"].toFixedDown(3));
+   $('.js-flatMagicDamage').text(arrayTotalStats["FlatMagicDamage"].toFixedDown(3));
+   $('.js-flatMagicDamage').attr('data-statsAmount',arrayTotalStats["FlatMagicDamage"].toFixedDown(3));
+   $('.js-flatPhysicalDamage').text(arrayTotalStats["FlatPhysicalDamage"].toFixedDown(3));
+   $('.js-flatPhysicalDamage').attr('data-statsAmount', arrayTotalStats["FlatPhysicalDamage"].toFixedDown(3));
+   $('.js-flatMovementSpeed').text(arrayTotalStats["FlatMovementSpeed"].toFixedDown(3));
+   $('.js-flatMovementSpeed').attr('data-statsAmount', arrayTotalStats["FlatMovementSpeed"].toFixedDown(3));
+   $('.js-percentMovementSpeed').text(arrayTotalStats["PercentMovementSpeed"].toFixedDown(3));
+   $('.js-percentMovementSpeed').attr('data-statsAmount', arrayTotalStats["PercentMovementSpeed"].toFixedDown(3));
+   $('.js-flatCritChance').text(arrayTotalStats["FlatCritChance"].toFixedDown(3));
+   $('.js-flatCritChance').attr('data-statsAmount', arrayTotalStats["FlatCritChance"].toFixedDown(3));
+   $('.js-flatArmor').text(arrayTotalStats["FlatArmor"].toFixedDown(3));
+   $('.js-flatArmor').attr('data-statsAmount', arrayTotalStats["FlatArmor"].toFixedDown(3));
+   $('.js-flatSpellBlock').text(arrayTotalStats["FlatSpellBlock"].toFixedDown(3));
+   $('.js-flatSpellBlock').attr('data-statsAmount', arrayTotalStats["FlatSpellBlock"].toFixedDown(3));
+   $('.js-flatHPPool').text(arrayTotalStats["FlatHPPool"].toFixedDown(3));
+   $('.js-flatHPPool').attr('data-statsAmount', arrayTotalStats["FlatHPPool"].toFixedDown(3));
+   $('.js-flatHPRegen').text(arrayTotalStats["FlatHPRegen"].toFixedDown(3));
+   $('.js-flatHPRegen').attr('data-statsAmount', arrayTotalStats["FlatHPRegen"].toFixedDown(3));
+   $('.js-flatMPPool').text(arrayTotalStats["FlatMPPool"].toFixedDown(3));
+   $('.js-flatMPPool').attr('data-statsAmount', arrayTotalStats["FlatMPPool"].toFixedDown(3));
+   $('.js-percentLifeSteal').text(arrayTotalStats["PercentLifeSteal"].toFixedDown(3));
+    $('.js-percentLifeSteal').attr('data-statsAmount', arrayTotalStats["PercentLifeSteal"].toFixedDown(3));
+}
+
+var setTotalStats = function(){
+  var arrayStats = [];
+  var arrayTotalStats = [];
+
+  JSON.parse(window.localStorage.getItem("actualBackPackItemsStats")).forEach( function(element) {
+    if(element!==null){
+      arrayStats.push(getItemStats(element));
+    }
+  });
+  arrayTotalStats = getTotalStats(arrayStats);
+  renderAllStats(arrayTotalStats);
+  getBaseStatsPerLevel($('.js-champlv').val());
+}
+
+$(document).ready( function(){  //events handlers
+  localStorage.removeItem("actualBackPackItemsStats");
+ 
+  var itemImageRoute ="http://ddragon.leagueoflegends.com/cdn/6.5.1/img/item/";
+  var totalItems = 0;
+  
+  var actualBackPack = [];
+  var backPackLength = 5;
+  var itemsTotalStats = { PercentAttackSpeed: 0, FlatMagicDamage:0, FlatPhysicalDamage: 0, 
+    FlatMovementSpeed: 0, PercentMovementSpeed: 0, FlatCritChance: 0, FlatArmor: 0, FlatSpellBlock: 0, 
+    FlatHPPool: 0, FlatHPRegen: 0, FlatMPPool: 0, PercentLifeSteal: 0 };
+  
+  $('.js-item').on('click', function(){
+    var actualBackPack = JSON.parse(window.localStorage.getItem("actualBackPackItemsStats")) || [];
+    //debugger;
+    var countNotNulls =0;
+    if(actualBackPack !== null){
+      actualBackPack.forEach( function(element) {
+        if(element !== null){
+          countNotNulls++;
+        }
+      });
+    }
+    //debugger;
+    if((actualBackPack !==null && countNotNulls <= 6)){
+      
+      for(var index = 0; index <= backPackLength; index++) {
+        if(actualBackPack[index]===undefined || actualBackPack[index]===null){
+          renderItemBackPackImage($(this).data('image'), index);
+          actualBackPack[index] = JSON.parse($(this).data('stats').replace(/=>/g, ":"));
+          countNotNulls++;
+          break;
+        }
+      }
+      window.localStorage.setItem("actualBackPackItemsStats", JSON.stringify(actualBackPack));
+      setTotalStats();
+    }
+    else{
+      console.log("Build completa");
+    }
+  });
+
+   
+  
+});
---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,16 @@
+require 'lol'
+class ItemsController < ApplicationController
+  def show
+    client = Lol::Client.new ENV["RIOT_DEV_API_KEY"], {region: "euw"}
+    #client = start_lol_client('euw')
+    all_items = client.static.item.get(itemListData:"all")
+    items_map11 = all_items.select{|item| item.maps['11'] == true }
+    no_required_champ = items_map11.select{|item| !item.requiredChampion }
+    #binding.pry
+    no_consumable = no_required_champ.select{|item| (item.tags == nil && item.name.include?('Enchantment'))||(item.tags != nil && !item.tags.include?('Consumable') && !item.tags.include?('Trinket'))}#&& ((!item.tags.include?('Trinket') && !item.tags.include?('Consumable')) && (!item.name.include?('Poro') && !item.name.include?('Biscuit'))) }
+    with_stats = no_consumable.select{|item| (item.tags && item.stats) || item.name == "Enchantment: Alacrity" || item.name == "Enchantment: warrior" || item.name == "Enchantment: Cinderhulk" || item.name == "Enchantment: Runic Echoes" || item.name == "Enchantment: Devourer" }
+    #no_items_with_bugs = with_stats.select{|item| ![3301,3672,3674,3673,3932,3930,3931,1041,3096,1006,30241,1004,3069,30241,3114,3241,3706,3711,3715,1039].include? item.id }
+    no_items_with_bugs = with_stats.select{|item| item.stats !=nil}
+    @items = no_items_with_bugs
+  end
+end
