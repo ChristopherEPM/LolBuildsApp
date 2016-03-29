a28ef04b112bddcacc5dc5b66f85e3661e965f4b
ChristopherEPM <bloodaxe00@hotmail.com>
2016-03-29 17:04:50 +0200

    modify the model to work with cron job it check if we have the same version as api if not we upgrade our version

--- app/models/gameversion.rb
+++ app/models/gameversion.rb
@@ -1,3 +1,19 @@
+require 'lol'
 class Gameversion < ActiveRecord::Base
   
+
+  def update_with_api
+    client = Lol::Client.new ENV["RIOT_DEV_API_KEY"], {region: "euw"}
+    api_version = client.static.versions.get[0]
+    local_version = Gameversion.first.version
+    if(api_version != local_version)
+      update_version(api_version,local_version)
+    end
+  end
+
+  def update_version(apiv,localv)
+    localv.update_attribute({
+      version: apiv
+    })
+  end
 end
