fa7cddbef1b11d3367c720bef84504517a9da075
ChristopherEPM <bloodaxe00@hotmail.com>
2016-03-29 16:39:02 +0200

    modified to obtain data from the db instead from the api

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,16 +1,18 @@
 require 'lol'
 class SiteController < ApplicationController
   
-  
-  
+
   def home
     
   end
 
   def show_champions_greed 
-    client = Lol::Client.new ENV["RIOT_DEV_API_KEY"], {region: "euw"}
-    @champions =  client.static.champion.get(champData: 'image')
-    render 'champions_grid'
+    #client = Lol::Client.new ENV["RIOT_DEV_API_KEY"], {region: "euw"}
+    # @champions =  client.static.champion.get(champData: 'image')
+    # @version = client.static.versions.get[0]
+    @champions = Champion.all
+    @version = Gameversion.first.version
+    render 'champions_grid' 
   end
 
 end
