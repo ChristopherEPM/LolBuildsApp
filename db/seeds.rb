0a62ce17e31cfa404d3f93fce552626874711d7a
ChristopherEPM <bloodaxe00@hotmail.com>
2016-04-05 17:49:20 +0200

    add minors changes to the champions &items controller, and the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,4 @@
 require 'lol'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# users = User.create([
-#   {name: 'Ian',  email: 'ian@dischord.com', password: '12345678', password_confirmation: '12345678'},
-#   {name: 'Henry',  email: 'hank@sst.com', password: '12345678', password_confirmation: '12345678'},
-#   {name: 'Glenn', email: 'danzig@plan9.com', password: '12345678', password_confirmation: '12345678'},
-#   {name: 'H.R.', email: 'paul@reachoutinternational.com', password: '12345678', password_confirmation: '12345678'}
-# ])
-
-# tasks = Task.create([
-#   {name: 'Task 1 for Ian',    due_date: DateTime.now, user_id: users[0].id},
-#   {name: 'Task 2 for Ian',    due_date: DateTime.now, user_id: users[0].id},
-#   {name: 'Task 1 for Henry',  due_date: DateTime.now, user_id: users[1].id},
-#   {name: 'Task 2 for Henry',  due_date: DateTime.now, user_id: users[1].id},
-#   {name: 'Task 1 for Glenn',  due_date: DateTime.now, user_id: users[2].id},
-#   {name: 'Task 2 for Glenn',  due_date: DateTime.now, user_id: users[2].id},
-#   {name: 'Task 1 for H.R.',   due_date: DateTime.now, user_id: users[3].id},
-#   {name: 'Task 2 for H.R',    due_date: DateTime.now, user_id: users[3].id},
-# ])
 
 client = Lol::Client.new ENV["RIOT_DEV_API_KEY"], {region: "euw"}
 champions =  client.static.champion.get(champData: 'all')
@@ -50,15 +25,3 @@ no_items_with_bugs.each do |item|
     api_id: item.id, name: item.name, group: item.group, description: item.description, sanitizedDescription: item.sanitizedDescription, depth: item.depth, from: item.from, into: item.into, maps: item.map, image: item.image["full"], stats: item.stats, gold: item.gold, effect: item.effect
     })
 end 
-
-# t.integer  "api_id"
-#     t.string   "name"
-#     t.string   "key"
-#     t.string   "image"
-#     t.text     "tags"
-#     t.string   "partype"
-#     t.text     "stats"
-#     t.text     "spells"
-#     t.text     "passive"
-#     t.datetime "created_at", null: false
-#     t.datetime "updated_at", null: false
